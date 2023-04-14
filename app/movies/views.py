8d9a707fed106042377dbe3d62746ede7d850e37
ajadi <ajadimarvellousgo@gmail.com>
2023-04-14 06:07:10 +0100

    final Update

--- app/movies/views.py
+++ app/movies/views.py
@@ -7,7 +7,7 @@ from rest_framework import viewsets
 # Create your views here.
 
 class MovieListView(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.all().order_by('-ranking') # Sort by ranking in descending order
     serializer_class = MovieSerializer
 
     # def get_extra_actions(self):
